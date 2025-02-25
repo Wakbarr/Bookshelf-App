015e01cb51814144e3fe5c8df111d3edac3c4e0b
Akbar Widianto <wdntoakbar@gmail.com>
2025-02-25 23:56:36 +0700

    feat: kriteria subs

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ const STORAGE_KEY = 'RND_BOOKSHELF';
 const SAVED_EVENT = 'saved-bookshelf';
 const RENDER_EVENT = 'render-bookshelf';
 
-// STORAGE LOGIC
 function isStorageExist() {
-  if (typeof (Storage) === undefined) {
+  if (typeof(Storage) === 'undefined') {
     toast('Browser kamu tidak mendukung local storage', 'danger');
     return false;
   }
@@ -33,15 +32,17 @@ function loadDataFromStorage() {
 
 function switchBookshelfStatus(bookId) {
   const book = findBook(bookId);
-  book.isComplete = !book.isComplete;
-  saveData();
-  document.dispatchEvent(new Event(RENDER_EVENT));
+  if (book) {
+    book.isComplete = !book.isComplete;
+    saveData();
+    document.dispatchEvent(new Event(RENDER_EVENT));
+  }
 }
 
-function removeBook(bookId){
-  const todoTarget = findBookIndex(bookId);
-  if (todoTarget === -1) return;
-  bookshelves.splice(todoTarget, 1);
+function removeBook(bookId) {
+  const index = findBookIndex(bookId);
+  if (index === -1) return;
+  bookshelves.splice(index, 1);
   saveData();
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
@@ -64,142 +65,130 @@ function findBookIndex(bookId) {
   return -1;
 }
 
-// DOM LOGIC
-
 const makeBook = (bookObject) => {
-  const {id, title, author, year, isComplete} = bookObject;
-
-  const container = document.createElement('article');
-  container.setAttribute('id', `book-${id}`);
-  container.classList.add('book');
-
+  const { id, title, author, year, isComplete } = bookObject;
+  const container = document.createElement('div');
+  container.setAttribute('data-bookid', id);
+  container.setAttribute('data-testid', 'bookItem');
+  container.classList.add('book-entry');
   const infoContainer = document.createElement('div');
-  infoContainer.classList.add('book_info');
-
-  const bookTitle = document.createElement('h4');
-  bookTitle.classList.add('book_title');
+  infoContainer.classList.add('book-info');
+  const bookTitle = document.createElement('h3');
+  bookTitle.setAttribute('data-testid', 'bookItemTitle');
   bookTitle.innerText = title;
-
   const bookAuthor = document.createElement('p');
-  bookAuthor.classList.add('book_author');
-  bookAuthor.innerText = `Author: ${author}`;
-
+  bookAuthor.setAttribute('data-testid', 'bookItemAuthor');
+  bookAuthor.innerText = 'Penulis: ' + author;
   const bookYear = document.createElement('p');
-  bookYear.classList.add('book_year');
-  bookYear.innerText = `Publication Year: ${year}`;
-
+  bookYear.setAttribute('data-testid', 'bookItemYear');
+  bookYear.innerText = 'Tahun: ' + year;
   infoContainer.append(bookTitle, bookAuthor, bookYear);
-
   const buttonContainer = document.createElement('div');
   buttonContainer.classList.add('book_action');
-
   const statusButton = document.createElement('button');
+  statusButton.setAttribute('data-testid', 'bookItemIsCompleteButton');
   statusButton.classList.add('btn', 'btn-sm', 'rounded-0');
-  statusButton.classList.add(isComplete ? 'btn-secondary' : 'btn-success');
-  statusButton.innerHTML = isComplete ? '<i class="bi bi-bookmark-dash"></i>' : '<i class="bi bi-bookmark-check"></i>';
-  statusButton.title = 'Tambahkan ke list ' + (isComplete ? 'Belum selesai dibaca' : 'Selesai dibaca');
+  if (isComplete) {
+    statusButton.classList.add('btn-secondary');
+    statusButton.innerHTML = '<i class="bi bi-bookmark-dash"></i>';
+    statusButton.title = 'Pindahkan ke Rak Belum selesai dibaca';
+  } else {
+    statusButton.classList.add('btn-success');
+    statusButton.innerHTML = '<i class="bi bi-bookmark-check"></i>';
+    statusButton.title = 'Pindahkan ke Rak Selesai dibaca';
+  }
   statusButton.addEventListener('click', () => {
     switchBookshelfStatus(id);
-    toast(`Buku <b>${title} (${year})</b> berhasil dipindahkan ke <i>Rak ${isComplete ? 'Belum selesai dibaca' : 'Selesai dibaca'}</i>`, 'success');
+    toast(`Buku <b>${title} (${year})</b> berhasil dipindahkan`, 'success');
   });
-  
-  const trashButton = document.createElement('button');
-  trashButton.classList.add('btn', 'btn-danger', 'btn-sm', 'rounded-0');
-  trashButton.innerHTML = '<i class="bi bi-trash"></i>';
-  trashButton.title = 'Hapus buku';
-  trashButton.addEventListener('click', () => {
-    if(confirm(`Apakah kamu yakin ingin menghapus buku ${title} (${year})?`)){
+  const deleteButton = document.createElement('button');
+  deleteButton.setAttribute('data-testid', 'bookItemDeleteButton');
+  deleteButton.classList.add('btn', 'btn-danger', 'btn-sm', 'rounded-0');
+  deleteButton.innerHTML = '<i class="bi bi-trash"></i>';
+  deleteButton.title = 'Hapus Buku';
+  deleteButton.addEventListener('click', () => {
+    if (confirm(`Apakah kamu yakin ingin menghapus buku ${title} (${year})?`)) {
       removeBook(id);
       toast(`Buku <b>${title} (${year})</b> berhasil dihapus`, 'success');
     }
   });
-  
-  buttonContainer.append(statusButton, trashButton);
-
-  container.append(infoContainer);
-  container.append(buttonContainer);
-
+  const editButton = document.createElement('button');
+  editButton.setAttribute('data-testid', 'bookItemEditButton');
+  editButton.classList.add('btn', 'btn-secondary', 'btn-sm', 'rounded-0');
+  editButton.innerText = 'Edit Buku';
+  buttonContainer.append(statusButton, deleteButton, editButton);
+  container.append(infoContainer, buttonContainer);
   return container;
-}
+};
 
 const searchBook = (keyword) => {
-  const bookItem = document.querySelectorAll(".book");
-  for (const item of bookItem) {
-      const title = item.querySelector(".book_title");
-      if (title.innerText.toUpperCase().includes(keyword.toUpperCase())) {
-          title.parentElement.parentElement.style.display = "";
-      } else {
-          title.parentElement.parentElement.style.display = "none";
-      }
+  const bookItems = document.querySelectorAll(".book-entry");
+  for (const item of bookItems) {
+    const title = item.querySelector("[data-testid='bookItemTitle']");
+    if (title.innerText.toUpperCase().includes(keyword.toUpperCase())) {
+      item.style.display = "";
+    } else {
+      item.style.display = "none";
+    }
   }
 };
 
 const generateId = () => {
   return +new Date();
-}
+};
 
 const generateBookshelfObject = (id, title, author, year, isComplete) => {
-  return {id, title, author, year, isComplete}
-}
+  return { id, title, author, year, isComplete };
+};
 
 function addBookshelf() {
   const bookID = generateId();
-  const bookTitle = document.getElementById('inputBookTitle').value;
-  const bookAuthor = document.getElementById('inputBookAuthor').value;
-  const bookYear = parseInt(document.getElementById('inputBookYear').value);
-  const bookIsComplete = document.getElementById('inputBookIsComplete').checked;
-
+  const bookTitle = document.getElementById('bookFormTitle').value;
+  const bookAuthor = document.getElementById('bookFormAuthor').value;
+  const bookYear = parseInt(document.getElementById('bookFormYear').value);
+  const bookIsComplete = document.getElementById('bookFormIsComplete').checked;
   const bookshelfObject = generateBookshelfObject(bookID, bookTitle, bookAuthor, bookYear, bookIsComplete);
   bookshelves.push(bookshelfObject);
-
   saveData();
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  const formInputBook = document.getElementById('inputBook');
+  const formInputBook = document.getElementById('bookForm');
   const formSearchBook = document.getElementById('searchBook');
-
   if (isStorageExist()) {
-    loadDataFromStorage()
+    loadDataFromStorage();
   }
-
   formInputBook.addEventListener('submit', (e) => {
     e.preventDefault();
     addBookshelf();
     e.target.reset();
     toast('Buku berhasil ditambahkan', 'success');
   });
-
   formSearchBook.addEventListener('input', (e) => {
     e.preventDefault();
     searchBook(e.target.value);
   });
-  
   formSearchBook.addEventListener('submit', (e) => {
     e.preventDefault();
   });
-
 });
 
 document.addEventListener(SAVED_EVENT, () => {
-  console.log('Data berhasil di simpan.');
+  console.log('Data berhasil disimpan.');
 });
 
 document.addEventListener(RENDER_EVENT, () => {
-  const incompleteBookshelfList = document.getElementById('incompleteBookshelfList');
-  const completeBookshelfList = document.getElementById('completeBookshelfList');
-
-  // clearing list item
+  const incompleteBookshelfList = document.getElementById('incompleteBookList');
+  const completeBookshelfList = document.getElementById('completeBookList');
   incompleteBookshelfList.innerHTML = '';
   completeBookshelfList.innerHTML = '';
-
   for (const book of bookshelves) {
-    const todoElement = makeBook(book);
+    const bookElement = makeBook(book);
     if (book.isComplete) {
-      completeBookshelfList.append(todoElement);
+      completeBookshelfList.append(bookElement);
     } else {
-      incompleteBookshelfList.append(todoElement);
+      incompleteBookshelfList.append(bookElement);
     }
   }
-})
+});
--- toast.js
+++ toast.js
@@ -1,39 +1,32 @@
 const toast = (msg, type = null) => {
-    const toast = document.createElement('div');
-    toast.classList.add('toast', 'align-items-center', 'mb-3');
-    toast.setAttribute('role', 'alert');
-    toast.setAttribute('data-bs-delay', '3000');
-  
-    switch (type) {
-      case 'success':
-        toast.classList.add('text-bg-success');
-        break;
-      case 'danger':
-        toast.classList.add('text-bg-danger');
-        break;
-      default:
-        toast.classList.add('text-bg-secondary');
-        break;
-    }
-  
-    const toastBody = document.createElement('div');
-    toastBody.classList.add('d-flex');
-    
-    const toastContent = document.createElement('div');
-    toastContent.classList.add('toast-body');
-    toastContent.innerHTML = msg;
-    toastBody.append(toastContent);
-    
-    const toastButton = document.createElement('button');
-    toastButton.setAttribute('type', 'button');
-    toastButton.classList.add('btn-close', 'me-2', 'm-auto', 'btn-close-white');
-    
-    toastButton.setAttribute('data-bs-dismiss', 'toast');
-    toastBody.append(toastButton);
-  
-    toast.append(toastBody);
-  
-    const toastContainer = document.getElementById('toastR');
-    toastContainer.append(toast);
-    new bootstrap.Toast(toast).show()
+  const toastElem = document.createElement('div');
+  toastElem.classList.add('toast', 'align-items-center', 'mb-3');
+  toastElem.setAttribute('role', 'alert');
+  toastElem.setAttribute('data-bs-delay', '3000');
+  switch (type) {
+    case 'success':
+      toastElem.classList.add('text-bg-success');
+      break;
+    case 'danger':
+      toastElem.classList.add('text-bg-danger');
+      break;
+    default:
+      toastElem.classList.add('text-bg-secondary');
+      break;
   }
+  const toastBody = document.createElement('div');
+  toastBody.classList.add('d-flex');
+  const toastContent = document.createElement('div');
+  toastContent.classList.add('toast-body');
+  toastContent.innerHTML = msg;
+  toastBody.append(toastContent);
+  const toastButton = document.createElement('button');
+  toastButton.setAttribute('type', 'button');
+  toastButton.classList.add('btn-close', 'me-2', 'm-auto', 'btn-close-white');
+  toastButton.setAttribute('data-bs-dismiss', 'toast');
+  toastBody.append(toastButton);
+  toastElem.append(toastBody);
+  const toastContainer = document.getElementById('toastR');
+  toastContainer.append(toastElem);
+  new bootstrap.Toast(toastElem).show();
+};
